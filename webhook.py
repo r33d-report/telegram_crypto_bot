e222ab1c9528930f73c6306b3b520bf79e7ac707
r33d-report <jwig24@gmail.com>
2025-05-14 13:02:47 -0400

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,1 +1,19 @@
+from flask import Flask, request
+import os
+import subprocess
 
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo_path = "/root/telegram_crypto_bot"
+        try:
+            subprocess.run(['git', '-C', repo_path, 'pull'], check=True)
+            subprocess.run(['pm2', 'restart', 'crypto-bot'], check=True)
+            return 'Updated and restarted', 200
+        except subprocess.CalledProcessError as e:
+            return f'Error: {e}', 500
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
